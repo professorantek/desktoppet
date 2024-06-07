93663a580626705f12972277cc0c4eb410697139
polykacz gowna <achicken.mcbite@gmail.com>
2024-06-07 20:01:23 +0200

    Comments and code clarity

--- Character.h
+++ Character.h
@@ -19,30 +19,37 @@ class Character{
     public:
         /**
          * Function to set the path for the animation files
-         * \param path string containing path to an animation file
-         * \param r  SDL_Renderer*
+         * \param path `string` containing path to an animation file
+         * \param r `SDL_Renderer*`
          * \returns true - if the path was set and animation was initialized; false - if one of them was not 
         */
         bool SetAnimation(std::string path, SDL_Renderer *r);
+
         /**
          * Function that moves the character around the screen
+         * \param w width of the window
+         * \param h height of the window
         */
         void Move(int w, int h);
+
         void SetHoldPoint();
+
         /**
          * Function to move the character to a defined position
-         * \param x1,y1 position of the mouse at SetHoldPoint() - used to calculate the right position after moving
+         * \param x1,y1 position of the mouse at `SetHoldPoint()` call time - used to calculate the right position after moving
          * \param x2,y2 current position of the mouse
         */
         void ForceMove(int x1, int y1, int x2, int y2);
+
         /**
          * Function to set the right frame of the animation
-         * \param now int of current time
+         * \param now `int` of current time
         */
         void Animate(int now);
+        
         /**
-         * Function displays the character on the screen
-         * \param r SDL_Renderer*
+         * Function that displays the character on the screen
+         * \param r `SDL_Renderer*`
         */
         void Display(SDL_Renderer* r);
 };
--- main.cpp
+++ main.cpp
@@ -7,11 +7,10 @@
 const int DISPLAYW = 1920, DISPLAYH = 1080;
 const char* title = "WindowPet";
 std::string animationPath = "animation.gif";
-int animationDelays = 100;
-int lastTime = 0;
-bool holding = false;
-SDL_Window* window= nullptr;
-SDL_Renderer* renderer= nullptr;
+int lastTime = 0, x, y;
+bool var = true, holding = false;
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
 Character c;
 
 void CreateWindowAndRenderer(){
@@ -22,6 +21,7 @@ void CreateWindowAndRenderer(){
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     SDL_RenderSetScale(renderer, 1,1);
 }
+
 bool MakeWindowTransparent(SDL_Window* window, COLORREF colorKey) {
     SDL_SysWMinfo wmInfo;
     SDL_VERSION(&wmInfo.version);
@@ -31,61 +31,69 @@ bool MakeWindowTransparent(SDL_Window* window, COLORREF colorKey) {
     return SetLayeredWindowAttributes(hWnd, colorKey, 0, LWA_COLORKEY);
 }
 
-int main(){
-    CreateWindowAndRenderer();
-    MakeWindowTransparent(window, RGB(255,0,255));
-    c.SetAnimation(animationPath ,renderer);
-    bool var = true;
+void EventProcesser(){
+    int x2,y2;
+    SDL_Event event;
+    if(SDL_PollEvent(&event)){
+        if(event.type == SDL_KEYDOWN){
+            switch(event.key.keysym.sym){
+                case SDLK_ESCAPE:{
+                    var = false;
+                    break;
+                }
+            }
+        }
+        if(event.type == SDL_MOUSEBUTTONDOWN){
+            switch(event.button.button){
+                case SDL_BUTTON_LMASK:{
+                    SDL_GetMouseState(&x, &y);
+                    c.SetHoldPoint();
+                    holding = true;
+                    break;
+                }
+            }
+        }
+        if(event.type == SDL_MOUSEBUTTONUP){
+            switch(event.button.button){
+                case SDL_BUTTON_LMASK:{
+                    holding = false;
+                    break;
+                }
+            }
+        }
+        if(event.type == SDL_MOUSEMOTION){
+            if(holding){
+                SDL_GetMouseState(&x2, &y2);
+                c.ForceMove(x, y, x2, y2);
+            }
+        }
+    }
+}
+
+void Loop(){
     while(var){
         while(SDL_GetTicks64()-lastTime<TICKS_PER_FRAME){
             SDL_Delay(1);
         }
         lastTime = SDL_GetTicks64();
-        int x,y, x2, y2;
-        SDL_Event event;
-        if(SDL_PollEvent(&event)){
-            if(event.type == SDL_KEYDOWN){
-                switch(event.key.keysym.sym){
-                    case SDLK_ESCAPE:{
-                        var = false;
-                        break;
-                    }
-                }
-            }
-            if(event.type == SDL_MOUSEBUTTONDOWN){
-                switch(event.button.button){
-                    case SDL_BUTTON_LMASK:{
-                        SDL_GetMouseState(&x, &y);
-                        c.SetHoldPoint();
-                        holding = true;
-                        break;
-                    }
-                }
-            }
-            if(event.type == SDL_MOUSEBUTTONUP){
-                switch(event.button.button){
-                    case SDL_BUTTON_LMASK:{
-                        holding = false;
-                        break;
-                    }
-                }
-            }
-            if(event.type == SDL_MOUSEMOTION){
-                if(holding){
-                    SDL_GetMouseState(&x2, &y2);
-                    c.ForceMove(x, y, x2, y2);
-                }
-            }
-        }
+        EventProcesser();
         SDL_SetRenderDrawColor(renderer, 255,0,255,255);
         SDL_RenderClear(renderer);
         c.Animate(SDL_GetTicks64());
-        if(rand()%20==0){
+        if(rand()%20==0&&!holding){
             c.Move(DISPLAYW, DISPLAYH);
         }
         c.Display(renderer);
         SDL_RenderPresent(renderer);
     }
+    
+}
+
+int main(){
+    CreateWindowAndRenderer();
+    MakeWindowTransparent(window, RGB(255,0,255));
+    c.SetAnimation(animationPath ,renderer);
+    Loop();
     SDL_DestroyWindow(window);
     SDL_Quit();
     return EXIT_SUCCESS;
